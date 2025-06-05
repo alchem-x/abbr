0df00ccca90b6220c90dadaaeaececac66f5cf49
阿尔贝鲁 <cbdyzj@gmail.com>
2025-06-05 22:11:59 +0800

    Update sort.js

--- src/sort.js
+++ src/sort.js
@@ -1,11 +1,10 @@
 import { readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import YAML from 'yaml'
-import { entries, flow, fromPairs } from 'lodash-es'
-
-const sortKeyDict = (arr) => arr.sort(([a], [b]) => a.localeCompare(b))
+import { entries, flow, fromPairs, sortBy } from 'lodash-es'
 
 const filePath = join(import.meta.dirname, 'abbr.yaml')
 const abbrStr = await readFile(filePath, 'utf8')
-const newAbbrStr = flow(YAML.parse, entries, sortKeyDict, fromPairs, YAML.stringify)(abbrStr)
+const dictSort = (arr) => sortBy(arr, 0)
+const newAbbrStr = flow(YAML.parse, entries, dictSort, fromPairs, YAML.stringify)(abbrStr)
 await writeFile(filePath, newAbbrStr)
